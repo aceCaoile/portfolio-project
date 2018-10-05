ac662609a461bde0cfa23cd1772809247b259240
aceCaoile <ace_caoile@hotmail.com>
2018-10-05 19:09:30 +0800

    Added bootstrap and jobs are showing up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Create a blog model here.
+# title
+# pub_date
+# body
+# image
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()  # "auto_now_add" when True will auto fill the Date and Time with now...
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
+# Add the blog app to the settings
+
+# Create a migration
+
+# Migrate
+
+# Add to the admin
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import jobs.views  # Go to jobs app/folder and import the views.py from that app/folder
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', jobs.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
