59c3e5e643b65bff2a2d65607b3efdfc84624ecf
aceCaoile <ace_caoile@hotmail.com>
2018-10-16 02:28:34 +0800

    Edited job details page

--- jobs/models.py
+++ jobs/models.py
@@ -6,13 +6,29 @@ class Job(models.Model):
     pub_date = models.DateTimeField()  # "auto_now_add" when True will auto fill the Date and Time with now...
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    body2 = models.TextField(default='', blank=True)
     image2 = models.ImageField(default=None, blank=True, upload_to='images/')
+
+    body3 = models.TextField(default='', blank=True)
     image3 = models.ImageField(default=None, blank=True, upload_to='images/')
+
+    body4 = models.TextField(default='', blank=True)
     image4 = models.ImageField(default=None, blank=True, upload_to='images/')
+
+    body5 = models.TextField(default='', blank=True)
     image5 = models.ImageField(default=None, blank=True, upload_to='images/')
+
+    body6 = models.TextField(default='', blank=True)
     image6 = models.ImageField(default=None, blank=True, upload_to='images/')
+
+    body7 = models.TextField(default='', blank=True)
     image7 = models.ImageField(default=None, blank=True, upload_to='images/')
+
+    body8 = models.TextField(default='', blank=True)
     image8 = models.ImageField(default=None, blank=True, upload_to='images/')
+
+    body9 = models.TextField(default='', blank=True)
     image9 = models.ImageField(default=None, blank=True, upload_to='images/')
 
     def __str__(self):  # This is the name of the function used to display titles from the admin page
